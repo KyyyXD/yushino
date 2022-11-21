d8bf5bdd1a37dfae0c5846e402abdade2a39f4dd
KyyyXD <117956036+KyyyXD@users.noreply.github.com>
2022-11-21 12:20:25 +0800

    Update bkp.js

--- plugins/bkp.js
+++ plugins/bkp.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 	const sukses = './src/avatar_contact.png'
- conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jngn comly  ya', m)
+ conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'nambah dosa yah banh?🗿', m)
  conn.sendFile(m.chat, sukses, 'attp.webp', '', m, false, { asSticker: true })
 }
 handler.help = ['bkp']
